1a6735cb690e957c7193329d3d53b7615af2b71d
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2022-12-14 13:26:55 -0300

    Reproduce slither bug

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol" as OZ;
+
 contract Counter {
     uint256 public number;
+
+    constructor() {
+        new OZ.ERC721("My NFT", "NFT");
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
